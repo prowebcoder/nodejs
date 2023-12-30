652a08c4ccd49fff679ba081140ff854d0203927
Prowebcoder <rahul@prowebcoder.com>
2023-12-30 12:22:27 +0530

    Comman Page or Sections in EJS

--- template-engine/index.js
+++ template-engine/index.js
@@ -1,30 +1,34 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const dirPath = path.join(__dirname, 'inner');
-app.set('view engine', 'ejs');
+const dirPath = path.join(__dirname, "inner");
+app.set("view engine", "ejs");
 // app.use(express.static(dirPath));
 
-app.get('/profile',(req, res) => {
-    const user = {
-        name:"Rahul",
-        email:"rahul@prowebcoder.com",
-        contact:1234567890
-    }
-    res.render('profile',{user:user});
-})
-app.get('/',(req, res) => {
-    res.sendFile(`${dirPath}/index.html`);
-})
-app.get('/about',(req, res) => {
-    res.sendFile(`${dirPath}/about.html`);
-})
-app.get('/contact',(req, res) => {
-    res.sendFile(`${dirPath}/contact.html`);
-})
-app.get('*',(req, res) => {
-    res.sendFile(`${dirPath}/404.html`);
-})
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "Rahul",
+    email: "rahul@prowebcoder.com",
+    contact: 1234567890,
+    skills: ["php", "javascript", "node"],
+  };
+  res.render("profile", { user: user });
+});
+app.get("/login", (req, res) => {
+  res.render("login",);
+});
+app.get("/", (req, res) => {
+  res.sendFile(`${dirPath}/index.html`);
+});
+app.get("/about", (req, res) => {
+  res.sendFile(`${dirPath}/about.html`);
+});
+app.get("/contact", (req, res) => {
+  res.sendFile(`${dirPath}/contact.html`);
+});
+app.get("*", (req, res) => {
+  res.sendFile(`${dirPath}/404.html`);
+});
 
 app.listen(5000);
