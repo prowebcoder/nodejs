cd28ae582a106229944f790732ee5ff19153e704
Prowebcoder <rahul@prowebcoder.com>
2024-01-01 12:19:10 +0530

    Mongo DB connection

---
+++ mongodbconnect/index.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const database = "prowebcoder";
+const client = new MongoClient(url);
+async function getData() {
+  let result = await client.connect();
+  let db = result.db(database);
+  let collection = db.collection("products");
+  let response = await collection.find({}).toArray();
+  console.log(response);
+}
+getData();
